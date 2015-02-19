a461de6cf4d9ca218faf4d8316de8a15955e431d
plamen1982 <plamen.hristov@ymail.com>
2015-02-19 16:12:46 -0500

    The AppFunc

--- KatanaBasics/Program.cs
+++ KatanaBasics/Program.cs
@@ -1,36 +1,57 @@
-﻿using Microsoft.Owin.Hosting;
+﻿
+using Microsoft.Owin.Hosting;
 using Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace KatanaBasics
 {
+   
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
     class Program
     {
         static void Main(string[] args)
         {
             string uri = "http://localhost:8080";
-            using (WebApp.Start<Startup>(uri))
+            using (WebApp.Start<Startup>(uri)) 
             {
-                Console.WriteLine("The server is running at localhost:8080");
+                Console.WriteLine("Your server is running at 8080");
                 Console.ReadKey();
-                Console.WriteLine("You just stop the server");
+                Console.WriteLine("The server now is not running");
             }
+
         }
+
     }
-    public class Startup
-    {
+
+    public class Startup { 
         public void Configuration(IAppBuilder app)
         {
-            app.UseWelcomePage();
-
-            //app.Run(ctx =>
-            //{
-            //   return  ctx.Response.WriteAsync("Hello World");
-            //});
+            app.Use<HelloWorldComponents>();
         }
     }
+
+    public class HelloWorldComponents
+    { 
+        AppFunc _next;
+        public HelloWorldComponents(AppFunc next)
+        {
+            _next = next;
+        }
+        public Task Invoke(IDictionary<string, object> environment)
+        
+        {
+            var response = environment["owin.ResponseBody"] as Stream;
+            using (var writer = new StreamWriter(response)) 
+            {
+                return writer.WriteAsync("Hello!!");
+            };
+        }
+    }
+
 }
